d1e917ad8fa73fd13ef1acfb7921bb1ba36cd12c
Matheus Amorim <rodriguesamorim0@gmail.com>
2024-03-28 14:41:29 -0300

    sexto commit do js e AGORA SIM PROJETO FINALIZADO

--- script.js
+++ script.js
@@ -60,6 +60,28 @@ function calcular() {
         strongGorjetaTotal.innerHTML = `R$ ${(conta * gorjeta / pessoas).toFixed(2)}`
 
         const strongTotal = document.querySelector(".total > strong")
-        strongTotal.innerHTML = (conta + (conta * porcentagem)) / pessoas
+        strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}`
     }
+}
+
+const botaoLimpar = document.querySelector (".resultados button")
+botaoLimpar.addEventListener ("click", limpar)
+
+function limpar () {
+    contaInput.value = ""
+
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+    })
+
+    gorjetaInput.value = ""
+
+    pessoasInput.value = ""
+
+    document.querySelector("gorjeta-total > strong").innerHTML = "R$ 0.00"
+    document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+    conta = 0
+    porcentagem = 0
+    pessoas = 0
 }
