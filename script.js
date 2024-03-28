64f2791b342dec5569e29abb6bd5548d706d8c86
Matheus Amorim <rodriguesamorim0@gmail.com>
2024-03-28 13:29:08 -0300

    setimo commit do html e primeiro commit do js

---
+++ script.js
@@ -0,0 +1,25 @@
+let conta = 0
+let pessoas = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener ("input", )
+
+function receberValorConta (evento) {
+   conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener ("input", )
+
+function receberQuantidadePessoas (evento) {
+   const paragrafoErro = document.querySelector(".pessoas #erro")
+   const divErro = document.querySelector(".pessoas .input-box")
+
+   if(evento.target.value == "0") {
+    paragrafoErro.styles.display = "block"
+    divErro.setAttribute("id", "erro-div")
+   } else {
+    paragrafoErro.styles.display = "none"
+    divErro.setAttribute("id", "")
+   }
+}
